f4a5854a6839cb99ad676add7a9a9494fa92a607
sohila samir <sohila.samir1911@gmail.com>
2022-03-20 18:43:29 +0200

    circleci testing

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -28,7 +28,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("hello world!");
+    res.send("hello world");
   });
 
   // Start the Server
